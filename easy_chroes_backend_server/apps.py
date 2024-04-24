19939ad1db1ed4efc5ed81a3d3bb2ff73763494e
ianlauyin <ianlauyin@gmail.com>
2024-04-24 22:35:17 +0800

    Set up route for app

---
+++ easy_chroes_backend_server/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EasyChroesBackendServerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'easy_chroes_backend_server'
---
+++ easy_chroes_backend_server/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+def first_function(request):
+    return HttpResponse("Hello")
+# Create your views here.
