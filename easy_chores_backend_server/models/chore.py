a55872cdd4cf1a451f233d6572880131b9851290
IanLau <ianlauyin@gmail.com>
2024-05-15 22:31:51 +0800

    Update Chore Model

--- easy_chores_backend_server/models/chore.py
+++ easy_chores_backend_server/models/chore.py
@@ -6,10 +6,10 @@ class Chore(models.Model):
     group = models.ForeignKey(
         Group, on_delete=models.CASCADE, related_name='chores')
     assigned_users = models.ManyToManyField(
-        User, related_name='chores')
+        User, related_name='chores', blank=True)
     title = models.CharField(max_length=100)
     detail = models.TextField(max_length=500, default="")
-    completed_date = models.DateField(null=True)
+    completed_date = models.DateField(null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
